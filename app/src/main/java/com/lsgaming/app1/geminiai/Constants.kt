a38628b9a29e5e75f0f689c0508ad84b120cd96a
LSGAMING281004 <lsgaming342@gmail.com>
2025-02-07 21:44:33 -0800

    Initial commit

---
+++ app/src/main/java/com/lsgaming/app1/geminiai/Constants.kt
@@ -0,0 +1,5 @@
+package com.lsgaming.app1.geminiai
+
+object Constants {
+    val apiKey = "AIzaSyD-pagNJWBa8OVbolk-h4XEWO9QAxrM02A"
+}
---
+++ app/src/main/java/com/lsgaming/app1/geminiai/MessageModel.kt
@@ -0,0 +1,6 @@
+package com.lsgaming.app1.geminiai
+
+data class MessageModel(
+    var message: String,
+    var role: String,
+)
